e40fd30cfc27a94938f2d8b2c6b4dc2a4d59e08d
Michal <michmiku@gmail.com>
2020-06-02 01:51:41 +0200

    about me text

--- src/components/home-screen/AboutMe.js
+++ src/components/home-screen/AboutMe.js
@@ -14,7 +14,7 @@ const AboutMe = ({ enterViewPort, exitViewPort }) => {
         <div className={currentClass}>
             <h1>About Me</h1>
             <div className="aboutme-text">
-                <p>My name is Michał Mikulski. I have been programming since I started attending high school. I am comfortable and take joy in coding front-end and back-end applications. Nonetheless I am just a beginner and I am constantly learning new skills and mastering the ones i have.</p>
+                <p>My name is Michał Mikulski. I have been programming since I started attending high school. I am comfortable and take joy in coding front-end and back-end applications. Nonetheless I am just a beginner and I am constantly learning new skills and mastering the ones I have.</p>
             </div>
             <div className="aboutme-schools">
                 <h2>Education</h2>
