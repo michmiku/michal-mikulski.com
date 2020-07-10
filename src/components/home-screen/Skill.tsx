72e32aa8896c891c29417ca2fd00e9712d5fe70c
Michal <michmiku@gmail.com>
2020-07-10 22:35:40 +0200

    login input position

--- src/components/home-screen/Skill.tsx
+++ src/components/home-screen/Skill.tsx
@@ -21,7 +21,7 @@ const skillsHover = {
     hover: {
         scale: 1.2,
         textShadow: "0px 0px 8px rgba(0,0,0,0.5)",
-        transition: { delay: 0.2 }
+        transition: { delay: 0 }
     },
     animate: {
         x: 0,
